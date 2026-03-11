efe64f9f29013bdf42bb1b56cdd7fc005bcd5a14
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-03-11 11:55:51 +0600

    Add PUT endpoint to update user details by ID

--- src/server.ts
+++ src/server.ts
@@ -114,6 +114,35 @@ app.get('/users/:id', async (req: Request, res: Response) => {
   }
 });
 
+app.put('/users/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(`UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`,
+      [name, email, id]
+    );
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: 'User not found'
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: 'User updated successfully',
+        data: result.rows[0]
+      });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
