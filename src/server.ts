53bc6d24b58e06f6973c6dcdfb8a50d869bd8480
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-03-11 12:33:35 +0600

    Add 404 error handling for undefined routes

--- src/server.ts
+++ src/server.ts
@@ -208,6 +208,15 @@ app.get('/todos', async (req: Request, res: Response) => {
   }
 });
 
+// not found route
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+    path: req.path
+  });
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
