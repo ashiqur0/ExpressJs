c66fbd6143ddb93eccd61f885438847ea3ae0e1d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-03-12 06:30:00 +0600

    Update .gitignore, add build script to package.json, remove logs.txt, and create vercel.json for deployment

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
-import fs from 'fs';
+// import fs from 'fs';
 
 // logger middleware
 const logger = (req: Request, res: Response, next: NextFunction) => {
   const log = `[${new Date().toISOString()}] ${req.method} ${req.path}\n`;
 
-  fs.writeFileSync('./src/logs.txt', log, { flag: 'a' });
+  // fs.writeFileSync('./src/logs.txt', log, { flag: 'a' });
 
   next();
 }
